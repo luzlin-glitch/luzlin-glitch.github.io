9bf0cd2e389563417aa8d7e631d76048d3069315
luzlin-glitch <nini111333@gmail.com>
2026-06-07 17:39:31 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ $(window).ready(function() {
         $("nav > p:not(:first-of-type)").slideToggle();
     });
 
-    $("nav a").on("click", function() {
-        if ($(window).width() <= 1200) {
-            $("nav > p:not(:first-of-type)").slideUp();
-        }
+    $(".album-item").on("click", function() {
+        let imageText = $(this).attr("data-images");
+        let imageList = imageText.split(",");
+
+        $("#viewer-content").html("");
+        $("#viewer-content").append('<p class="viewer-note">Click Close to return to the portfolio.</p>');
+
+        imageList.forEach(function(imageName) {
+            $("#viewer-content").append('<img src="' + imageName + '" alt="portfolio photo">');
+        });
+
+        $("#photo-viewer").slideDown();
+    });
+
+    $("#close-viewer").on("click", function() {
+        $("#photo-viewer").slideUp();
     });
 });
